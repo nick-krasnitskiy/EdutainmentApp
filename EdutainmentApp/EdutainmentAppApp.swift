f142eaeaa84c54827df77f11d2db2a95e3efeed5
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2024-06-04 08:30:43 +0300

    Initial Commit

---
+++ EdutainmentApp/EdutainmentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentAppApp.swift
+//  EdutainmentApp
+//
+//  Created by Nick Krasnitskiy on 04.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
